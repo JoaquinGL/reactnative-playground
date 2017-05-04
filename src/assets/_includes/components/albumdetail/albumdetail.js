753eba01c8da6a483ff58fb70179eab8a2a7251e
Joaquin Giraldez Lopez <joaquing@hanzo.es>
2017-05-04 17:50:39 +0200

    add covert img

--- src/assets/_includes/components/albumdetail/albumdetail.js
+++ src/assets/_includes/components/albumdetail/albumdetail.js
@@ -1,19 +1,80 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image} from 'react-native';
 import Card from '../card/card';
 import CardSection from '../cardsection/cardsection';
 
 // Make a component
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+  const {
+    title,
+    artist,
+    thumbnail_image,
+    image
+  } = album;
+
+  const {
+    thumbnailStyle,
+    headerContenStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
   return(
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={ {uri: thumbnail_image} }/>
+        </View>
+        <View style={headerContenStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={ {uri: image} }/>
       </CardSection>
     </Card>
   );
 };
 
+const styles = {
+  headerContenStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+
+  headerTextStyle: {
+    fontSize: 18,
+  },
+
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+};
+
 // Make the component available to other parts of the app
 export default AlbumDetail;
+
+
+// (props) -> ( { album} ) -> when you have multiple references to props object
