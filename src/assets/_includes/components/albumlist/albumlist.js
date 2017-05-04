bf3d917a397cabd11fe05789e8c5adef4288246c
Joaquin Giraldez Lopez <joaquing@hanzo.es>
2017-05-04 16:05:42 +0200

    add it a render albums and state methods

--- src/assets/_includes/components/albumlist/albumlist.js
+++ src/assets/_includes/components/albumlist/albumlist.js
@@ -1,13 +1,34 @@
 // Import libraries for making a component
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 // Make a component
 class Albumlist extends Component {
+  state = { albums: [] } // init the state
+
+  componentWillMount() { // automatic call as soon as this component is render in the screen fantastic place to load some data
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums') //return a promise
+      .then (
+        response => this.setState({ albums: response.data}) // update state
+      );
+  }
+
+  // helper method
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text> // key must to be unique
+    );
+  }
+
   render(){
+
+    console.log(this.state);
+
   	return (
     	<View>
-      	<Text> Album list </Text>
+      	{this.renderAlbums()}
     	</View>
   	);
   }
